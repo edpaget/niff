72d0dfcf8e2610d23da02bf0f7c61063cce808f6
Edward Paget <ed.paget@gmail.com>
2014-03-20 16:05:26 -0500

    Add environments

---
+++ lib/niff/environment.rb
@@ -0,0 +1,46 @@
+require 'json'
+
+module Niff
+  class NoEnvironmentTypeSpecified < StandardError
+  end
+  
+  class EnvironmentBuilder
+    def initialize(name, type=nil) 
+      @name = name
+      @type = type
+    end
+
+    def subdomain(s)
+      @subdomain = s
+    end
+
+    def tld(t)
+      @tld = t
+    end
+
+    def type(t)
+      @type = t
+    end
+
+    def build
+      raise NoEnvironmentTypeSpecified.new unless @type
+      @type.new(@name, @subdomain, @tld)
+    end
+  end
+
+  module Environment
+    def qualify(domain)
+      tld = @tld || domain.tld
+      d = String.new
+      p @name
+      d.concat("." + @subdomain) if @subdomain
+      d.concat(".") 
+      d.concat(domain.name + "." + tld)
+    end
+
+    def create_environment
+      # override
+    end
+  end
+end
+
