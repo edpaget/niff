6d1db93c13724724dcb63ea28891630511e8692d
Edward Paget <ed.paget@gmail.com>
2014-03-20 08:18:58 -0500

    Remove logging

--- spec/unit/cluster_spec.rb
+++ spec/unit/cluster_spec.rb
@@ -5,7 +5,6 @@ describe Niff::Cluster do
 
   describe "#to_json" do
     it "should produce a valid json respentation" do
-      p cluster
       expect(cluster.to_json).to eq("{\"name\":\"name\",\"nodes\":[]}")
     end
   end
